3f5048d8a0f39839de0ea79024e8a1d227c9eeec
dronx86 <androncalgon@gmail.com>
2021-08-24 01:45:29 +0300

    Second commit, baby

---
+++ main.py
@@ -0,0 +1,80 @@
+import file_operations
+import os
+from faker import Faker
+import random
+
+INSERT = os.path.join("src", "charsheet.svg")
+
+SKILLS = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд"
+]
+
+RUNES = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def main():
+
+    for counter in range(10):
+
+        character = Faker("ru_RU")
+
+        generated_skills = random.sample(SKILLS, 3)
+        beautiful_skills = []
+
+        for skill in generated_skills:
+            for letter in skill:
+                skill = skill.replace(letter, RUNES[letter])
+            beautiful_skills.append(skill)
+
+        context = {
+            "first_name": character.first_name(),
+            "last_name": character.last_name(),
+            "job": character.job(),
+            "town": character.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": beautiful_skills[0],
+            "skill_2": beautiful_skills[1],
+            "skill_3": beautiful_skills[2]
+        }
+
+        result = os.path.join("res", "created{}.svg".format(counter+1))
+        file_operations.render_template(INSERT, result, context)
+
+
+if __name__ == "__main__":
+    main()
